91794796a6748a991a0cf2feb419d4df429cec45
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-08-10 13:58:07 +0300

    Update 125_valid_palindrome.py

--- Top Interview 150/easy/125_valid_palindrome.py
+++ Top Interview 150/easy/125_valid_palindrome.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def isPalindrome(self, s):
-        sentence_string = "".join([char.lower() for char in s if char.isalnum()])
-        return sentence_string == sentence_string[::-1]
+        s = "".join([char.lower() for char in s if char.isalnum()])
+        return s == s[::-1]
 
 solution = Solution()
 print(solution.isPalindrome("A man, a plan, a canal: Panama"))
